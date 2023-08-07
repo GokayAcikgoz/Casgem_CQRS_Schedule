ce6fce172c99011485f38fa8a29e4a7722368655
gokay <gokayacikgoz1@gmail.com>
2023-08-07 16:43:50 +0300

    Add project files.

---
+++ Project/CQRS/Handlers/GetEventQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Project.Models;
+
+namespace Project.CQRS.Handlers
+{
+    public class GetEventQueryHandler
+    {
+        private readonly CalendarContext _context;
+
+        public GetEventQueryHandler(CalendarContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Event> Handle(DateTime start, DateTime end)
+        {
+            var values = _context.Events.Where(e => !((e.End <= start) || (e.Start >= end)));
+
+
+            return values;
+        }
+    }
+}
